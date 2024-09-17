19e0c277d9069201071fa0e41663aed1d4d2b380
HeartLessSoul <heartlesssoul@heartlesssoul>
2024-09-17 23:13:10 +0000

    Following Patrick Collins to push

---
+++ script/DeployFundMe.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployFundMe is Script {
+    function run() external returns (FundMe) {
+        HelperConfig helpConfig = new HelperConfig();
+        address ethUsdPriceFeed = helpConfig.activeNetworkConfig();
+
+        vm.startBroadcast();
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
+        vm.stopBroadcast();
+        return fundMe;
+    }
+}
